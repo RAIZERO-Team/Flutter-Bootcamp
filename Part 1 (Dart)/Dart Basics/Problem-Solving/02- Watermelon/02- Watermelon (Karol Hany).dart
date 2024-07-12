cf98307a3a47cb153c580b1c25c5a710a2fb9f5b
karol <karolhany09@gmail.com>
2024-07-12 22:04:10 +0300

    ps 2 dart

---
+++ Part 1 (Dart)/Dart Basics/Problem-Solving/02- Watermelon/02- Watermelon (Karol Hany).dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  int weight = int.parse(stdin.readLineSync()!);
+
+  if (weight % 2 == 0 && weight > 2) {
+    print("YES");
+  } else {
+    print("NO");
+  }
+}
