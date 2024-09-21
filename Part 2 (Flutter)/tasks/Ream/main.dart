2412e664a0d91d4cab7696c7e892da42472bead9
Reem <reemezzatamin4@gmail.com>
2024-09-21 03:42:12 +0300

    splash screen task

--- Part 2 (Flutter)/tasks/Ream/main.dart
+++ Part 2 (Flutter)/tasks/Ream/main.dart
@@ -1,5 +1,8 @@
+// Author: Reem Ezzat
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+
+import 'splash_screen.dart';
 
 void main() {
   runApp(DevicePreview(
@@ -15,18 +18,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Home Page',
       debugShowCheckedModeBanner: false,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            'Test work',
-            style: TextStyle(fontSize: 50),
-          ),
-        ),
-      ),
+      home: const SplashScreen(),
     );
   }
 }
